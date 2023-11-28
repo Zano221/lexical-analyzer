eff01bc3870220e2b43098e67f63dc51a04ea064
Zano221 <zanopcwr@protonmail.com>
2023-11-28 16:38:53 -0300

    TODO: fix the same_word glitch problem (asd, aasd, aasde)

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+
+const DEFAULT_STATE = 0;
+const ALPHABET_SIZE = 26;
+
 let matrix = []; // M[STATE, SYMBOL] = NEW_STATE, EXEMPLO (string "abc"): M[0]["a"] = 1, M[1]["b"] = 2, M[2]["c"] = 3 (final),
 let wordList = [];
 let automata_length = 0;
 let table_length = 0;
 let finalStates = [];
 
-const DEFAULT_STATE = 0;
 let STATE = DEFAULT_STATE;
 
 
@@ -23,7 +26,8 @@ $( function() {
       console.log(word)
       
       if(findElementInArray(word) || word == "") return;
-      
+
+      if(!/^[a-z]+$/.test(word)) return;
 
       // INSERIR NA LISTA DE PALAVRAS EXISTENTES
       insertToList(word);
@@ -35,9 +39,52 @@ $( function() {
       updateTable(word);
     })
 
-    /*$("#botao-morrer").click(function() {
-      $("#table-body").empty();
-    })*/
+    $("#search-input").keyup(function() {
+      const word = $("#search-input").val().toLowerCase();
+      let selected_state = STATE = 0; // 0
+
+      $("#lexical-table tbody tr td").removeClass("correct-cell"); // remover todas as instancias de letras corretas no automato
+      $("#lexical-table tbody tr td").removeClass("wrong-cell"); // remover todas as instancias de letras incorretas no automato
+      $("#lexical-table tbody tr td").removeClass("selection-cell"); // remover cor da coluna selecionada em volta da palavra correta no automato
+
+      
+      for(let i = 0; i < word.length; i++) { // iterar sobre a palavra, sempre pegando a ultima palavra e vendo se o caminho dela é correto
+        $("#lexical-table tbody tr td").removeClass("correct-cell"); // remover todas as instancias de letras corretas no automato
+        $("#lexical-table tbody tr td").removeClass("wrong-cell"); // remover todas as instancias de letras incorretas no automato
+        $("#lexical-table tbody tr td").removeClass("selection-cell"); // remover a cor da coluna selecionada da palavra incorreta no automato
+
+        if(matrix[selected_state][word[i]] !== undefined) {
+          console.log("POG", `${matrix[selected_state][word[i]]}`);
+          console.log($(`#table-state-q${matrix[selected_state][word[i]]}`));
+          let table = $(`#table-state-q${matrix[selected_state][word[i]]}`)
+
+          table.addClass("correct-cell");
+
+          for(let j = 0; j < ALPHABET_SIZE; j++) {
+            const letter = String.fromCharCode('a'.charCodeAt(0) + j);
+            $(`.cell-q${selected_state}-${letter}`).addClass("selection-cell"); 
+          }
+
+          let state_ammount = matrix.length;
+          for(let j = 0; j < state_ammount; j++) {
+            
+            console.log(`.cell-q${j}-${word[i]}`);
+            $(`.cell-q${j}-${word[i]}`).addClass("selection-cell"); 
+          }
+
+          selected_state = matrix[selected_state][word[i]];
+          
+          
+
+        }
+        else {
+          console.log("UNPOG");
+          console.log(`cell-q${selected_state}-${word[i]}`);
+          $(`.cell-q${selected_state}-${word[i]}`).addClass("wrong-cell");
+          break;
+        }
+      }
+    });
 })
 
 
@@ -89,46 +136,25 @@ function updateTable() {
     
     $(`#${row_instance}`).append(`<td class='table-terminal-head'>${current_state}</td>`); // atribuir essa variavel do q{ESTADO} à coluna
 
-
-
     // Esse codigo abaixo, vai renderizar o resto das linhas da matriz, seguindo o alfabeto inteiro
-    for(let i = 0; i < 26; i++) {
+    for(let i = 0; i < ALPHABET_SIZE; i++) {
 
       let shown_state;
       let letter = String.fromCharCode('a'.charCodeAt(0) + i);
+      let _stateElement;
 
       if(matrix[current_position] === undefined || matrix[current_position][letter] === undefined) {
         shown_state = `-`
+        _stateElement = `<td class='table-cell cell-q${current_position}-${letter}'> ${shown_state} </td>`;
       }
       else {
         shown_state = `q${matrix[current_position][letter]}`;
+        _stateElement = `<td class='table-cell cell-q${current_position}-${letter}' id='table-state-${shown_state}'> ${shown_state} </td>`;
       }
 
-      let _stateElement = `<td class=table-terminal id='table-terminal-${current_state}'> ${shown_state} </td>`;
       $(`#${row_instance}`).append(_stateElement);
     }
-
-    
-  
-  
-    table_length++;
   }
-
-
-  // ADICIONAR A LINHA DE TERMINO
-
-  /*let final_state = `q${matrix[automata_length-1]["FINAL"]}`
-  row_instance++;
-  table_length++;
-  $('#table-body').append(`<tr id=${row_instance}></tr>`) // atribuir o o id da primeira coluna a cada linha novamente
-  let current_state = `q${table_length}`; // atribuir o valor q{ESTADO} à uma variavel q salva o estado
-  $(`#${row_instance}`).append(`<td class='table-terminal-head'>${current_state}</td>`); // atribuir essa variavel do q{ESTADO} à coluna
-
-  
-  const _finalElement = `<td class=table-terminal-final id='table-terminal-*${final_state}'> ${final_state} </td>`;
-  $(`#${row_instance}`).append(_finalElement);*/
-
-
 }
 
 
@@ -150,12 +176,18 @@ function appendToMatrix(word) {
     let letter = word[i];
     let current_position;
 
-    if(!same_word) current_position = automata_length;
+    if(!same_word)  {
+      console.log("LIXO QUEBROU EM = ", letter)
+      current_position = automata_length;
+    }
     else current_position = i;
 
+
+    let test_next_state;
+    let next_state;
     if(matrix[current_position] === undefined) { // Se não existe uma linha nessa posição, então cria uma nova, e define que a palavra obviamente não é a mesma
       same_word = false;
-      matrix.push(Array(26)); // Crio uma nova linha contendo todo o alfabeto, cada letra vai ser inserida ali
+      matrix.push(Array(ALPHABET_SIZE)); // Crio uma nova linha contendo todo o alfabeto, cada letra vai ser inserida ali
       current_position = matrix.length-1;
       matrix[current_position][letter] = current_position+1;
     } else {
@@ -164,16 +196,18 @@ function appendToMatrix(word) {
         matrix[current_position][letter] = matrix.length;
       }
     }
+
+    test_next_state = matrix[current_position][letter];
     automata_length = matrix.length
 
-    console.log(same_word);
-    console.log(current_position);
+    //console.log(current_position);
+
+    console.log("ESTADO: ", current_position, "LETRA: ", letter, "PROXIMO ESTADO:", test_next_state);
     
   }
 
   const final_state = matrix.length-1;
   matrix.push(Array(26)); 
-  matrix[final_state]["FINAL"] = 0;
   finalStates.push(final_state+1)
   automata_length++;
   //console.log(finalStates)
@@ -182,10 +216,14 @@ function appendToMatrix(word) {
 
 }
 
+function searchOnTable() {
+
+}
+
 function searchOnMatrix(letter) {
 
   //Ira usar o estado atual para pesquizar
-  let prev_state = STATE;
+
   if(matrix[STATE][letter] !== null) switchState(matrix[STATE][letter]);
 
 }
